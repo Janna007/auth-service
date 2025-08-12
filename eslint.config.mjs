45bfe1cea2bef9ef04dd105186801748c47d251a
Janna007 <jannakondeth5@gmail.com>
2025-08-12 16:27:37 +0530

    add error handler

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,23 +1,21 @@
-
-import eslint from '@eslint/js';
-import tseslint from 'typescript-eslint';
+import eslint from '@eslint/js'
+import tseslint from 'typescript-eslint'
 
 export default tseslint.config(
-  eslint.configs.recommended,
- tseslint.configs.recommendedTypeChecked,
-  {
-    languageOptions: {
-      parserOptions: {
-        projectService: true,
-        tsconfigRootDir: import.meta.dirname,
-      },
+    eslint.configs.recommended,
+    tseslint.configs.recommendedTypeChecked,
+    {
+        languageOptions: {
+            parserOptions: {
+                projectService: true,
+                tsconfigRootDir: import.meta.dirname,
+            },
+        },
+        rules: {
+            // 'no-console':'off',
+        },
     },
-    rules:{
-      // 'no-console':'off',
+    {
+        ignores: ['dist', 'node_modules', 'eslint.config.mjs'],
     },
-  },
-  {
-     ignores:["dist","node_modules","eslint.config.mjs"],
-  }
- 
-);
+)
--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,30 @@
-import express from 'express'
+import express, { NextFunction, Request, Response } from 'express'
+import logger from './config/logger'
+import { HttpError } from 'http-errors-enhanced'
 
 const app = express()
+
+app.get('/', (req, res) => {
+    res.sendStatus(401)
+})
+
+//global error handler
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+app.use((err: HttpError, req: Request, res: Response, next: NextFunction) => {
+    logger.error(err.message)
+    const statusCode = err.statusCode || 500
+
+    res.status(statusCode).json({
+        errors: [
+            {
+                type: err.name,
+                msg: err.message,
+                path: '',
+                location: '',
+            },
+        ],
+    })
+})
 
 export default app
