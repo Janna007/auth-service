62df3b6e0c89c61b9164587b39479f4045df485c
Janna007 <jannakondeth5@gmail.com>
2025-11-04 18:31:28 +0530

    add token generations

--- src/controllers/AuthController.ts
+++ src/controllers/AuthController.ts
@@ -1,7 +1,13 @@
+import fs from 'fs'
 import { NextFunction, Response } from 'express'
 import { RequestRegisterUser } from '../types'
 import { UserService } from '../services/UserService'
 import { Logger } from 'winston'
+import { validationResult } from 'express-validator'
+import { JwtPayload, sign } from 'jsonwebtoken'
+import path from 'path'
+import createHttpError from 'http-errors'
+import { Config } from '../config'
 
 export class AuthController {
     constructor(
@@ -15,6 +21,13 @@ export class AuthController {
         res: Response,
         next: NextFunction,
     ) {
+        //field validation
+        const result = validationResult(req)
+        // console.log("validation result",result)
+        if (!result.isEmpty()) {
+            return res.status(400).json({ errors: result.array() })
+        }
+
         const { firstName, lastName, email, password } = req.body
 
         this.logger.debug('data recieved from request body', {
@@ -23,7 +36,6 @@ export class AuthController {
             email,
             password: '****',
         })
-
         try {
             const userCreated = await this.userService.createUser({
                 firstName,
@@ -35,6 +47,53 @@ export class AuthController {
             this.logger.info('user registered successfully', {
                 id: userCreated.id,
             })
+
+            //create tokens
+            let privateKey: Buffer
+            try {
+                privateKey = fs.readFileSync(
+                    path.join(__dirname, '../../certs/private.pem'),
+                )
+            } catch {
+                const err = createHttpError(
+                    500,
+                    'error while reading private key',
+                )
+                next(err)
+                return
+            }
+
+            const payload: JwtPayload = {
+                sub: String(userCreated.id),
+                role: userCreated.role,
+            }
+
+            const accessToken = sign(payload, privateKey, {
+                algorithm: 'RS256',
+                expiresIn: '1hr',
+                issuer: 'auth-service',
+            })
+
+            const refreshToken = sign(payload, Config.REFRESH_TOKEN_SECRET!, {
+                algorithm: 'HS256',
+                expiresIn: '1y',
+                issuer: 'auth-service',
+            })
+
+            res.cookie('access_token', accessToken, {
+                httpOnly: true,
+                domain: 'localhost',
+                sameSite: 'strict',
+                maxAge: 1000 * 60 * 60, //1 hour
+            })
+
+            res.cookie('refresh_token', refreshToken, {
+                httpOnly: true,
+                domain: 'localhost',
+                sameSite: 'strict',
+                maxAge: 1000 * 60 * 60 * 24 * 365, //1 year
+            })
+
             res.status(201).json({ id: userCreated.id })
         } catch (error) {
             next(error)
--- tests/users/register.spec.ts
+++ tests/users/register.spec.ts
@@ -4,30 +4,31 @@ import { AppDataSource } from '../../src/config/data-source'
 import { User } from '../../src/entity/User'
 import { DataSource } from 'typeorm'
 import { roles } from '../../src/constants'
+import { isJwt } from '../utils'
 
 describe('POST /auth/register', () => {
-    describe('Registered Succesfully with 201', () => {
-        let dataSource: DataSource
+    let dataSource: DataSource
 
-        beforeAll(async () => {
-            try {
-                dataSource = await AppDataSource.initialize()
-            } catch (error) {
-                console.error('Database init error ❌:', error)
-                throw error // stop tests if DB fails
-            }
-        })
+    beforeAll(async () => {
+        try {
+            dataSource = await AppDataSource.initialize()
+        } catch (error) {
+            console.error('Database init error ❌:', error)
+            throw error // stop tests if DB fails
+        }
+    })
 
-        beforeEach(async () => {
-            //database truncate //clean database before each test cases
-            await dataSource.dropDatabase()
-            await dataSource.synchronize()
-        })
+    beforeEach(async () => {
+        //database truncate //clean database before each test cases
+        await dataSource.dropDatabase()
+        await dataSource.synchronize()
+    })
 
-        afterAll(async () => {
-            await dataSource.destroy()
-        })
+    afterAll(async () => {
+        await dataSource.destroy()
+    })
 
+    describe('All fields are given', () => {
         //test case 1
         it('should return 201', async () => {
             const userData = {
@@ -161,6 +162,193 @@ describe('POST /auth/register', () => {
             expect(response.status).toBe(400)
             const users = await userRepository.find()
             expect(users).toHaveLength(1)
+        })
+
+        it('should return the access token and refresh token in response', async () => {
+            const userData = {
+                firstName: 'janna',
+                lastName: 'jk',
+                email: 'jannakondeth5@gmail.com',
+                password: 'janna123',
+            }
+            const response = await request(app)
+                .post('/auth/register')
+                .send(userData)
+
+            interface Headers {
+                ['set-cookie']?: string[]
+            }
+
+            // 'set-cookie': [
+            //     'access_token=jannakondeth; Max-Age=3600; Domain=localhost; Path=/; Expires=Tue, 04 Nov 2025 12:00:28 GMT; HttpOnly; SameSite=Strict',
+            //     'refresh_token=jannakondeth; Max-Age=31536000; Domain=localhost; Path=/; Expires=Wed, 04 Nov 2026 11:00:28 GMT; HttpOnly; SameSite=Strict'
+            //   ],
+
+            let accessToken = null
+            let refreshToken = null
+
+            // expect(response.headers).toHaveProperty('set-cookie');
+
+            const cookies = (response.headers as Headers)['set-cookie'] || []
+
+            cookies.forEach((cookie) => {
+                if (cookie.startsWith('access_token=')) {
+                    accessToken = cookie.split(';')[0]?.split('=')[1]
+                }
+                if (cookie.startsWith('refresh_token=')) {
+                    refreshToken = cookie.split(';')[0]?.split('=')[1]
+                }
+            })
+
+            // console.log("accessToekn",accessToken)
+
+            expect(accessToken).not.toBeNull()
+            expect(refreshToken).not.toBeNull()
+
+            expect(isJwt(accessToken)).toBeTruthy()
+            expect(isJwt(refreshToken)).toBeTruthy()
+        })
+    })
+
+    describe('Missing Fields with 400', () => {
+        it('should return 400 if email field is missing', async () => {
+            const userData = {
+                firstName: 'janna',
+                lastName: 'jk',
+                email: '',
+                password: 'janna123',
+            }
+
+            const response = await request(app)
+                .post('/auth/register')
+                .send(userData)
+
+            expect(response.status).toBe(400)
+            expect(response.body).toHaveProperty('errors')
+            expect(
+                (response.body as Record<string, string>).errors?.length,
+            ).toBeGreaterThan(0)
+
+            const userRepository = dataSource.getRepository(User)
+            const users = await userRepository.find()
+
+            expect(users).toHaveLength(0)
+        })
+        it('should return 400 if firstName field is missing', async () => {
+            const userData = {
+                firstName: '',
+                lastName: 'jk',
+                email: 'jannakondeth5@gmail.com',
+                password: 'janna123',
+            }
+
+            const response = await request(app)
+                .post('/auth/register')
+                .send(userData)
+
+            expect(response.status).toBe(400)
+
+            const userRepository = dataSource.getRepository(User)
+            const users = await userRepository.find()
+
+            expect(users).toHaveLength(0)
+        })
+
+        it('should return 400 if lastName field is missing', async () => {
+            const userData = {
+                firstName: 'janna',
+                lastName: '',
+                email: 'jannakondeth5@gmail.com',
+                password: 'janna123',
+            }
+
+            const response = await request(app)
+                .post('/auth/register')
+                .send(userData)
+
+            expect(response.status).toBe(400)
+
+            const userRepository = dataSource.getRepository(User)
+            const users = await userRepository.find()
+
+            expect(users).toHaveLength(0)
+        })
+
+        it('should return 400 if password field is missing', async () => {
+            const userData = {
+                firstName: 'janna',
+                lastName: 'jk',
+                email: 'jannakondeth5@gmail.com',
+                password: '',
+            }
+
+            const response = await request(app)
+                .post('/auth/register')
+                .send(userData)
+
+            expect(response.status).toBe(400)
+
+            const userRepository = dataSource.getRepository(User)
+            const users = await userRepository.find()
+
+            expect(users).toHaveLength(0)
+        })
+    })
+
+    describe('Fields are not format', () => {
+        it('should trim the email', async () => {
+            const userData = {
+                firstName: 'janna',
+                lastName: 'jk',
+                email: ' janna@gmail.com ',
+                password: 'janna123',
+            }
+
+            await request(app).post('/auth/register').send(userData)
+
+            const userRepository = dataSource.getRepository(User)
+            const users = await userRepository.find()
+
+            expect(users[0]?.email).toBe('janna@gmail.com')
+        })
+
+        it('should return 400 status code if password length is less than 8 chars', async () => {
+            const userData = {
+                firstName: 'janna',
+                lastName: 'jk',
+                email: ' janna@gmail.com ',
+                password: 'jann',
+            }
+
+            const response = await request(app)
+                .post('/auth/register')
+                .send(userData)
+
+            expect(response.status).toBe(400)
+
+            const userRepository = dataSource.getRepository(User)
+            const users = await userRepository.find()
+
+            expect(users).toHaveLength(0)
+        })
+
+        it('should return 400 if it is not a valid email', async () => {
+            const userData = {
+                firstName: 'janna',
+                lastName: 'jk',
+                email: 'jannagmail',
+                password: 'janna123',
+            }
+
+            const response = await request(app)
+                .post('/auth/register')
+                .send(userData)
+
+            const userRepository = dataSource.getRepository(User)
+            const users = await userRepository.find()
+            expect(users).toHaveLength(0)
+
+            expect(response.status).toBe(400)
         })
     })
 })
