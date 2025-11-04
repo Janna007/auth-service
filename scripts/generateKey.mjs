e6937bb3b5e407baed3bafce3f06c85d88639c86
Janna007 <jannakondeth5@gmail.com>
2025-11-04 18:32:40 +0530

    formatted file

--- scripts/generateKey.mjs
+++ scripts/generateKey.mjs
@@ -1,27 +1,21 @@
-import { generateKeyPairSync } from "crypto";
+import { generateKeyPairSync } from 'crypto'
 import fs from 'fs'
 
-  
-  const {
-    publicKey,
-    privateKey,
-  } = generateKeyPairSync('rsa', {
+const { publicKey, privateKey } = generateKeyPairSync('rsa', {
     modulusLength: 2048,
 
     publicKeyEncoding: {
-      type: 'pkcs1',
-      format: 'pem',
+        type: 'pkcs1',
+        format: 'pem',
     },
     privateKeyEncoding: {
-      type: 'pkcs1',
-      format: 'pem',
-
+        type: 'pkcs1',
+        format: 'pem',
     },
-  });
+})
 
+console.log('privateKey', privateKey)
+console.log('publicKey', publicKey)
 
-  console.log("privateKey",privateKey)
-  console.log("publicKey",publicKey)
-
-  fs.writeFileSync('certs/private.pem',privateKey)
-  fs.writeFileSync('certs/public.pem',publicKey)
+fs.writeFileSync('certs/private.pem', privateKey)
+fs.writeFileSync('certs/public.pem', publicKey)
