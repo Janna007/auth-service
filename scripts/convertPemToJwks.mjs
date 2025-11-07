c15e6e9eb0b0c5e701a2bf669ef55adfdcdaf44f
Janna007 <jannakondeth5@gmail.com>
2025-11-07 15:14:32 +0530

    format files

--- scripts/convertPemToJwks.mjs
+++ scripts/convertPemToJwks.mjs
@@ -1,9 +1,8 @@
 import fs from 'fs'
 import rsaPemToJwk from 'rsa-pem-to-jwk'
- 
-var pem = fs.readFileSync('./certs/private.pem');
- 
-var jwk = rsaPemToJwk(pem, {use: 'sig'}, 'public');
 
+var pem = fs.readFileSync('./certs/private.pem')
 
-console.log(JSON.stringify(jwk));
+var jwk = rsaPemToJwk(pem, { use: 'sig' }, 'public')
+
+console.log(JSON.stringify(jwk))
